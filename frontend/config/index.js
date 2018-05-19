2bf50241504344daec388df215638c889327a01f
davidwroche <davidwroche@yahoo.co.uk>
2018-05-19 19:35:49 +0100

    removed eslint

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -23,10 +23,8 @@ module.exports = {
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
-    useEslint: true,
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
-    showEslintErrorsInOverlay: false,
 
     /**
      * Source Maps
@@ -45,10 +43,9 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
-
+    index: path.resolve(__dirname, '../../dist/index.html'),
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
